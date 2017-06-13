f670a5f0aeaa0f0f93f599f5fda4ad533712a72c
jimontoyag <icha9127@gmail.com>
2017-06-12 22:59:09 -0500

    :round_pushpin: Notificacion Facebook Piloto
    Notificación en facebook a los pasajeros cuando el piloto publica.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,18 +12,18 @@ import { FacebookService, FacebookInitParams } from 'ng2-facebook-sdk';
 export class LoginComponent implements OnInit {
 
 
-  cargando : boolean = true;  
+  cargando : boolean = true;
   msgs:any;
-  
+
   private token:string;
-  private usuario: firebase.UserInfo;   
-  
+  private usuario: firebase.UserInfo;
+
   constructor(private af: AngularFire, private fb: FacebookService,private router: Router, private ags: AuthService) { }
 
- ngOnInit() {    
+ ngOnInit() {
     let fbParams: FacebookInitParams = {
                                    appId: '1832413313701021',
-                                   version: 'v2.8'                                   
+                                   version: 'v2.8'
                                    };
     this.fb.init(fbParams);
     this.fb.getLoginStatus().then(res => {
@@ -34,56 +34,58 @@ export class LoginComponent implements OnInit {
         });
         } else{
         this.estadoFuera();
-      }  
-    }); 
+      }
+    });
  }
 
   private estadoFuera(){
         this.ags.logout;
         this.cargando = false;
   }
-  
+
   private cambiaEstadoSesion(auth:FirebaseAuthState, uid:string){
     if(auth == null){
         this.estadoFuera();
       }else{
         this.usuario = auth.facebook;
       if(!this.usuario.uid){
-        this.usuario = auth.auth.providerData[0];      
+        this.usuario = auth.auth.providerData[0];
       }
-      
+
         this.peteneceAGrupo('502606019798663').then(pertenece=>{
           if(pertenece){
             this.ags.login(this.usuario,this.token);
-            this.af.database.list('/usuarios').update(this.usuario.uid, { 
+            /*
+              this.af.database.list('/usuarios').update(this.usuario.uid, {
               nombre: this.usuario.displayName,
               fotoUrl:this.usuario.photoURL});
+              */
             this.cargando = false;
             let url = this.ags.redirectUrl;
              if(url != null){
-               this.router.navigate([url]);       
+               this.router.navigate([url]);
              }else{
-              this.router.navigate(['/inicio']);    
+              this.router.navigate(['/inicio']);
              }
           }else{
-            this.cargando = false;   
-            this.showMsg('error',':(','Para poder acceder, debes ser mienbro del grupo Expreso Ibagué Péguese la Rodadita');       
-          }                         
+            this.cargando = false;
+            this.showMsg('error',':(','Para poder acceder, debes ser mienbro del grupo Expreso Ibagué Péguese la Rodadita');
+          }
           }).catch(err=>{
-            this.cargando = false; 
+            this.cargando = false;
             this.showMsg('error',':(*****',err);
-          });                        
-               
-      } 
+          });
+
+      }
   }
-  
+
   private peteneceAGrupo(grupo:string): Promise<boolean>{
     return this.fb.api('/'+grupo+'/members?access_token='+this.token+'&limit=100000').then(res => {
       var pertenece:boolean = false;
       res.data.forEach(member =>{
         if(this.usuario.uid == member.id){
           pertenece = true;
-        } 
+        }
       });
       return pertenece;
     });
@@ -95,8 +97,8 @@ export class LoginComponent implements OnInit {
         this.msgs.push({severity:severity, summary:summary, detail:detail});
   }
 
-   login() {         
-       this.af.auth.login();           
+   login() {
+       this.af.auth.login();
   }
 
 }
--- src/app/pilotos/pilotos.component.ts
+++ src/app/pilotos/pilotos.component.ts
@@ -2,6 +2,10 @@ import { Ubicacion } from '../modelo/ubicacion';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { LugaresService } from '../servicios/lugares.service';
+import { AuthService } from '../servicios/auth.service';
+import { Router } from '@angular/router';
+import { FacebookService} from 'ng2-facebook-sdk';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pilotos',
@@ -16,7 +20,10 @@ export class PilotosComponent implements OnInit {
   hora: number;
   descripcion: string;
 
-  constructor(private database: AngularFireDatabase, private lugaresService: LugaresService) { }
+  private notifica: boolean = false;
+
+  constructor(private database: AngularFireDatabase, private lugaresService: LugaresService, private router: Router, private fb: FacebookService, private authService: AuthService,
+    private datePipe: DatePipe) { }
 
   ngOnInit() {
   }
@@ -27,6 +34,44 @@ export class PilotosComponent implements OnInit {
 
   getServicioNombre(codigo:string):string{
     return this.lugaresService.getServicioNombre(codigo);
+  }
+
+  publicarCupo(){
+    let urlEvento = '/cupos/'+this.origen+'/'+this.destino+'/'+this.saleFecha.getFullYear()+'/'+this.saleFecha.getMonth()+'/'+this.saleFecha.getDate();
+    this.database.list(urlEvento)
+      .push({descripcion:this.descripcion, hora:this.hora, piloto:this.authService.usuario.uid, nombrePiloto:this.authService.usuario.displayName, fotoUrl:this.authService.usuario.photoURL})
+        .then(res => {
+          let arr = {}, uid =res.path.o[6] ;
+          arr[uid]={fecha:this.saleFecha.getFullYear()+'/'+this.saleFecha.getMonth()+'/'+this.saleFecha.getDate(),orDes:this.origen+'/'+this.destino};
+          this.database.list('/usuarios/'+this.authService.usuario.uid)
+            .update('cupos',arr)
+              .then(res =>{
+                this.notifica = true;
+                this.database.list(urlEvento+'/notifica').subscribe(usuarios =>{
+                  if(this.notifica){
+                    usuarios.forEach(usuario =>{
+                      this.notificaFacebook(usuario.$key);
+                    });
+                    this.notifica = false;
+                  }
+                });
+              }).catch(err => {
+                console.log(err);
+              });
+        }).catch(err =>{
+          console.log(err);
+        });
+  }
+
+  private notificaFacebook(usuario:string){
+    console.log('NOTIFICO A: '+usuario);
+    let fecha = this.datePipe.transform(this.saleFecha, 'EEEE , d MMMM');
+    let fechaSimple = this.datePipe.transform(this.saleFecha, 'yMMdd');
+    this.fb.api('/'+usuario+'/notifications?access_token=1832413313701021|54563aae7dfe0c79994ab744e643c842&href=pasajero?fecha%3D'+fechaSimple+'&template=Hay un cupo disponible para el '+fecha,'post');
+  }
+
+  irInicio(){
+    this.router.navigate(['/inicio']);
   }
 
 }
