72fcca3566c0f3ce4644fcf9fa7e6d65acef8080
leonardo merchan <leonardo.merchan.1992@gmail.com>
2017-06-23 11:08:51 -0500

    cambios interfaz

--- src/app/pasajeros/pasajeros.component.ts
+++ src/app/pasajeros/pasajeros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { LugaresService } from '../servicios/lugares.service';
+import { AuthService } from '../servicios/auth.service';
 @Component({
   selector: 'app-pasajeros',
   templateUrl: './pasajeros.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasajerosComponent implements OnInit {
 
-  constructor() { }
+cupos: FirebaseListObservable<any[]>;
+fecha: Date;
+origen: string;
+destino: string;
+
+
+  constructor(private database : AngularFireDatabase, private lugaresService: LugaresService, private authService: AuthService ) { }
 
   ngOnInit() {
+  }
+  lugares() : any[]{
+    return this.lugaresService.lugares;
+  }
+
+  busquedaCupo(){
+  this.cupos = this.database.list('/cupos/'+'/'+this.origen+'/'+this.destino+'/'+this.fecha.getFullYear()+'/'+this.fecha.getMonth()+'/'+this.fecha.getDate());
+  }
+  getServicioNombre(codigo:string):string{
+    return this.lugaresService.getServicioNombre(codigo);
   }
 
 }
